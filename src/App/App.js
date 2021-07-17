4f4d4d6f77fa5d5a23000d8f8df4a4ae662d8f2b
andriy-lanyk <LAM1988@ukr.net>
2021-07-17 20:36:48 +0300

    Add all functions

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,133 @@
-import React from "react";
+import React, { Component } from "react";
+import shortid from "shortid";
 
-const App = () => {
-  return <div>"Привет!"</div>;
-};
+class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+    name: "",
+    number: "",
+  };
+
+  getValue = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState((prevState) => (prevState[name] = value));
+  };
+
+  getContact = (e) => {
+    e.preventDefault();
+    const { contacts } = this.state;
+    const doubleContact = contacts.find(
+      (item) => item.name === e.target[0].value
+    );
+    if (doubleContact) {
+      alert(`${doubleContact.name} is already in contacts`);
+      return;
+    }
+    this.setState(({ contacts, name, number }) => {
+      return contacts.push({ id: shortid.generate(), name, number });
+    });
+    this.resetState();
+  };
+
+  findContact = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState((prevState) => (prevState[name] = value));
+    this.setState((prevState) => {
+      const arrayOfContacts = [...prevState.contacts];
+      prevState.contacts = arrayOfContacts.filter((item) =>
+        item.name.toLowerCase().includes(value.toLowerCase())
+      );
+    });
+  };
+
+  deleteContact = (e) => {
+    const target = e.target.name;
+    console.log("target: ", target);
+    this.setState((prevState) => {
+      prevState.contacts = prevState.contacts.filter((item) => {
+        return item.id !== target;
+      });
+    });
+    console.log(this.state.contacts);
+  };
+
+  resetState = () => {
+    return this.setState(
+      (prevState) =>
+        (prevState = {
+          contacts: [...prevState.contacts],
+          filter: "",
+          name: "",
+          number: "",
+        })
+    );
+  };
+
+  render() {
+    const { name, contacts, number, filter } = this.state;
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <form onSubmit={this.getContact}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              required
+              onChange={this.getValue}
+              value={name}
+            />
+          </label>
+          <label>
+            Number
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+              required
+              value={number}
+              onChange={this.getValue}
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <h2>Contacts</h2>
+        <label>
+          Find contacts by Name
+          <input
+            type="text"
+            name="filter"
+            value={filter}
+            onChange={this.findContact}
+          />
+        </label>
+        {contacts && (
+          <ul>
+            {contacts.map(({ id, name, number }) => (
+              <li key={id}>
+                {name}: {number}{" "}
+                <button name={id} onClick={this.deleteContact}>
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </>
+    );
+  }
+}
 
 export default App;
